7870f2b352e0cea54d6f1b7fe2b1a136895706fd
yancy <yancy@yancy.lol>
2022-05-21 14:24:56 +0200

    Find the most common words

--- src/main.rs
+++ src/main.rs
@@ -10,34 +10,62 @@ use core::cmp::Ordering;
 use std::cmp::Reverse;
 
 #[derive(Debug, Eq, PartialEq, Ord)]
-struct WordCount {
+struct WordMeta {
     word: String,
     count: u8,
 }
 
-impl PartialOrd for WordCount {
+struct Words {
+    words: Vec<WordMeta>
+}
+
+impl Words {
+    fn new(word_table: HashMap<String, u8>) -> Self {
+        let mut words: Vec<WordMeta> = Vec::new();
+
+        let mut words_vec = word_table
+            .iter()
+            .collect::<Vec<(&String, &u8)>>();
+
+        for w in words_vec {
+          let word_meta = WordMeta {
+            word: w.0.to_string(),
+            count: *w.1
+          };
+
+          words.push(word_meta);
+        }
+
+        Self { words }
+    }
+
+    fn sort(&mut self) {
+        self.words
+            .sort_unstable_by_key(
+                |item| (Reverse(item.count), item.word.clone())
+            )
+    }
+
+    fn find_most_common_words(
+        &mut self,
+        k: usize,
+        most_common_words: &mut Vec<String>) {
+        self.sort();
+
+        for i in 0..k {
+            let w = self.words[i].word.clone();
+            most_common_words.push(w);
+        }
+    }
+}
+
+impl PartialOrd for WordMeta {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         if self.count == other.count {
             return Some(self.word.cmp(&other.word));
         }
         Some(self.count.cmp(&other.count))
     }
-}
-
-fn build_sorted_vec(word_table: HashMap<String, u8>, word_vec: &mut Vec<WordCount>) {
-    let mut sort_word_vec = word_table
-        .iter()
-        .collect::<Vec<(&String, &u8)>>();
-
-    for w in sort_word_vec {
-      let word_count = WordCount {
-        word: w.0.to_string(),
-        count: *w.1
-      };
-      word_vec.push(word_count);
-    }
-
-    word_vec.sort_unstable_by_key(|item| (Reverse(item.count), item.word.clone()))
 }
 
 fn build_word_table(words: String, mut word_table: HashMap<String, u8>) -> 
@@ -79,171 +107,125 @@ mod tests {
         )
     }
 
+    fn word_count_three() -> Vec<&'static str> {
+        vec![
+            "to",
+            "you"
+        ]
+    }
+
+    fn word_count_two() -> Vec<&'static str> {
+        vec![
+            "a",
+            "are",
+            "how",
+            "nobody",
+            "tell"
+        ]
+    }
+
+    fn word_count_one() -> Vec<&'static str> {
+        vec![
+            "admiring",
+            "advertise",
+            "an",
+            "be",
+            "bog",
+            "dont",
+            "dreary",
+            "frog",
+            "im",
+            "june",
+            "know",
+            "like",
+            "livelong",
+            "name",
+            "of",
+            "ones",
+            "pair",
+            "public",
+            "somebody",
+            "the",
+            "then",
+            "theres",
+            "theyd",
+            "too",
+            "us",
+            "who"
+        ]
+    }
+
     #[test]
     fn build_word_table_test() {
         let word_table = HashMap::new();
         let word_table = build_word_table(quote(), word_table);
 
-        let count = word_table.get("im").unwrap();
-        assert_eq!(*count, 1 as u8);
+        for w in word_count_three() {
+            let count = word_table.get(w).unwrap();
+            assert_eq!(*count, 3 as u8);
+        }
 
-        let count = word_table.get("I'm");
-        assert_eq!(None, count);
+        for w in word_count_two() {
+            let count = word_table.get(w).unwrap();
+            assert_eq!(*count, 2 as u8);
+        }
 
-        let count = word_table.get("goldenrod");
-        assert_eq!(None, count);
-
-        let count = word_table.get("nobody").unwrap();
-        assert_eq!(*count, 2 as u8);
+        for w in word_count_one() {
+            let count = word_table.get(w).unwrap();
+            assert_eq!(*count, 1 as u8);
+        }
 
         let count = word_table.get("");
         assert_eq!(None, count);
     }
 
     #[test]
-    fn build_sorted_vec_test() {
-        let expected_occurence = vec![
-            WordCount {
-                word: "to".to_string(),
-                count: 3
-            },
-            WordCount {
-                word: "you".to_string(),
-                count: 3,
-            },
-            WordCount {
-                word: "a".to_string(),
-                count: 2,
-            },
-            WordCount {
-                word: "are".to_string(),
-                count: 2,
-            },
-            WordCount {
-                word: "how".to_string(),
-                count: 2,
-            },
-            WordCount {
-                word: "nobody".to_string(),
-                count: 2,
-            },
-            WordCount {
-                word: "tell".to_string(),
-                count: 2,
-            },
-            WordCount {
-                word: "admiring".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "advertise".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "an".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "be".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "bog".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "dont".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "dreary".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "frog".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "im".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "june".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "know".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "like".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "livelong".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "name".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "of".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "ones".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "pair".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "public".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "somebody".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "the".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "then".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "theres".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "theyd".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "too".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "us".to_string(),
-                count: 1,
-            },
-            WordCount {
-                word: "who".to_string(),
-                count: 1,
-            }
-        ];
-
+    fn sort_word_list_test() {
         let word_table = HashMap::new();
-        let mut sorted_vec = Vec::new();
         let word_table = build_word_table(quote(), word_table);
-        build_sorted_vec(word_table, &mut sorted_vec);
+        let mut words = Words::new(word_table);
+        words.sort();
+        
+        let mut expected = Vec::new();
 
-        for i in 0..expected_occurence.len() {
-            assert_eq!(expected_occurence[i], sorted_vec[i]);
+        for w in word_count_three() {
+            let word_meta = WordMeta { word: w.to_string(), count: 3 };
+            expected.push(word_meta);
         }
+
+        for w in word_count_two() {
+            let word_meta = WordMeta { word: w.to_string(), count: 2 };
+            expected.push(word_meta);
+        }
+
+        for w in word_count_one() {
+            let word_meta = WordMeta { word: w.to_string(), count: 1 };
+            expected.push(word_meta);
+        }
+
+        assert_eq!(expected, words.words);
+    }
+
+    #[test]
+    fn find_most_common_words_test() {
+        let word_table = HashMap::new();
+
+        let words: Vec<WordMeta> = Vec::new();
+        let mut words = Words { words };
+
+        let word_table = build_word_table(quote(), word_table);
+        let mut words = Words::new(word_table);
+
+        let mut most_common_words: Vec<String> = Vec::new();
+        words.find_most_common_words(1, &mut most_common_words);
+        assert_eq!(vec!["to"], most_common_words);
+
+        let mut most_common_words: Vec<String> = Vec::new();
+        words.find_most_common_words(2, &mut most_common_words);
+        assert_eq!(vec!["to", "you"], most_common_words);
+
+        let mut most_common_words: Vec<String> = Vec::new();
+        words.find_most_common_words(3, &mut most_common_words);
+        assert_eq!(vec!["to","you","a"], most_common_words);
     }
 }
